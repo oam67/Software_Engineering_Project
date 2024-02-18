3963faee1dcf90c8bcb14fe73048c3ab9685d1df
unknown <henry.koeller@slu.edu>
2024-02-18 14:44:02 -0600

    Update Game Screen with Timer

--- ContinentSelection.js
+++ ContinentSelection.js
@@ -7,7 +7,35 @@ document.addEventListener('DOMContentLoaded', function() {
         button.textContent = continent;
         button.addEventListener('click', function() {
             document.body.innerHTML = '';
+            displayTimer();
         });
         container.appendChild(button);
     });
+
+    function displayTimer() {
+        const timer = document.createElement('div');
+        document.body.appendChild(timer);
+
+        timeLeft = 60;
+
+        function updateTimer() {
+            timer.innerHTML = 'Timer: ' + timeLeft + ' seconds';
+
+            if (timeLeft <= 10) {
+                timer.style.color = 'red';
+            }
+
+            if (timeLeft === 0){
+                clearInterval(timerInterval);
+            }
+            else {
+                timeLeft --;
+            }
+        }
+        const timerInterval = setInterval(updateTimer, 1000);
+        
+        timer.style.position = 'fixed';
+        timer.style.top = '10px';
+        timer.style.right = '10px';
+    }
 });
