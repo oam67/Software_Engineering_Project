ffa0d6a1e162bcb16445bb232b36266e2a4f1d3d
Krishna Pothuganti <kspothuganti@gmail.com>
2024-02-19 17:54:54 -0600

    added guess box for user input - still need to work on handling the guess

--- ContinentSelection.js
+++ ContinentSelection.js
@@ -1,14 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const continents = ['Africa', 'Asia', 'Oceania', 'Europe', 'North America', 'South America'];
     const container = document.getElementById('buttonContainer');
 
     continents.forEach(continent => {
         const button = document.createElement('button');
         button.textContent = continent;
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             document.body.innerHTML = '';
             displayTimer();
             displayImage('United States of America.png');
+            displayGuessBox();
         });
         container.appendChild(button);
     });
@@ -26,15 +27,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 timer.style.color = 'red';
             }
 
-            if (timeLeft === 0){
+            if (timeLeft === 0) {
                 clearInterval(timerInterval);
             }
             else {
-                timeLeft --;
+                timeLeft--;
             }
         }
         const timerInterval = setInterval(updateTimer, 1000);
-        
+
         timer.style.position = 'fixed';
         timer.style.top = '10px';
         timer.style.right = '10px';
@@ -51,4 +52,29 @@ document.addEventListener('DOMContentLoaded', function() {
         image.style.transform = 'translate(-50%, -50%)';
         document.body.appendChild(image);
     }
+
+    function displayGuessBox() {
+        const guessBox = document.createElement('input');
+        guessBox.type = 'text';
+        guessBox.placeholder = 'Enter your guess...';
+        guessBox.id = 'userInput';
+
+        const subButton = document.createElement('button');
+        subButton.textContent = 'Submit Guess';
+        subButton.addEventListener('click', function () {
+            const userInput = guessBox.value;
+            console.log('User guess:', userInput);
+        });
+
+        const container = document.createElement('div');
+        container.appendChild(guessBox);
+        container.appendChild(subButton);
+
+        document.body.appendChild(container); container.style.position = 'absolute';
+
+        container.style.top = '70%';
+        container.style.left = '50%';
+        container.style.transform = 'translate(-50%, -50%)';
+    }
+
 });
