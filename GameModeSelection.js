56fc2ae7e42b81ee94ff1b7e9c56b5aee26b5955
Pranavi Kolouju <pranavikolouju@Pranavis-MacBook-Pro.local>
2024-03-31 22:05:08 -0500

    adjusting pictures for easy mode

--- GameModeSelection.js
+++ GameModeSelection.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const easyModeButton = document.getElementById('easyModeButton');
     const hardModeButton = document.getElementById('hardModeButton');
 
-    const selectMode = () => {
+    const selectMode = (mode) => {
+        sessionStorage.setItem('gameMode', mode);
+
         document.getElementById('gameModeButtons').style.display = 'none';
         window.location.href = 'ContinentSelection.html';
     };
 
-    easyModeButton.addEventListener('click', selectMode);
-    hardModeButton.addEventListener('click', selectMode);
+    easyModeButton.addEventListener('click', () => selectMode('easy'));
+    hardModeButton.addEventListener('click', () => selectMode('hard'));
 });
