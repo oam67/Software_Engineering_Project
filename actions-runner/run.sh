74f36ac047c273f430eac4a2e9a9f687bd1301f9
Oam Khatavkar <oam@Oams-MacBook-Air.local>
2024-04-11 16:21:00 -0500

    Removing certain files and directories that are no longer needed.

--- actions-runner/run.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b09964445fb7f75cc8ecb735cdf9bd1144ddc4349c0a80b5d07c2a2a79c3c90e
-size 2537
--- actions-runner/safe_sleep.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:457547876c4aaffa9cf3cb6ffbd93acf67b75dd73f0460165f7abc19bf66826f
-size 65
